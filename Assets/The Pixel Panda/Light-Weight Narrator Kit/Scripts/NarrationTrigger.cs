d5d1c1880a26511b8126e8ede6cd70032d2dcc90
ThePixelPandaGames <toni.antonio.palumbo@gmail.com>
2023-08-26 13:42:28 +0200

    added doc

--- Assets/The Pixel Panda/Light-Weight Narrator Kit/Scripts/NarrationTrigger.cs
+++ Assets/The Pixel Panda/Light-Weight Narrator Kit/Scripts/NarrationTrigger.cs
@@ -5,10 +5,76 @@ namespace LightWeightNarrationTool
 {
     public class NarrationTrigger : MonoBehaviour
     {
+        enum TriggerEvent
+        {
+            OnStart,
+            OnTriggerEnter,
+            OnCollisionEnter,
+            OnMouseClick,
+            OnTriggerEnter2D,
+            OnCollisionEnter2D
+        }
+
+
         [SerializeField]
         private NarrationSequence narrationSequence;
 
+        [SerializeField]
+        private TriggerEvent triggerEvent;
+
         void Start()
+        {
+            if (triggerEvent == TriggerEvent.OnStart)
+            {
+                TriggerNarration();
+            }
+        }
+
+        private void OnMouseDown()
+        {
+            if (triggerEvent == TriggerEvent.OnMouseClick)
+            {
+                TriggerNarration();
+            }
+        }
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (triggerEvent == TriggerEvent.OnTriggerEnter)
+            {
+                TriggerNarration();
+
+            }
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (triggerEvent == TriggerEvent.OnCollisionEnter)
+            {
+                TriggerNarration();
+
+            }
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if (triggerEvent == TriggerEvent.OnTriggerEnter2D)
+            {
+                TriggerNarration();
+
+            }
+        }
+
+        private void OnCollisionEnter2D(Collision2D collision)
+        {
+            if (triggerEvent == TriggerEvent.OnCollisionEnter2D)
+            {
+                TriggerNarration();
+
+            }
+        }
+
+        private void TriggerNarration()
         {
             try
             {
